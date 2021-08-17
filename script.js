359c0c583861daf47491003bf3962cea333661a5
NataliiaKudaieva <81326193+NataliiaKudaieva@users.noreply.github.com>
2021-08-17 22:50:15 +0300

    homework fix

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-var red = prompt("Please give me value for red parameter") || 0;
-var green = prompt("Please give me value for green parameter") || 0;
-var blue = prompt("Please give me value for blue parameter") || 0;
+// var red = prompt("Please give me value for red parameter") || 0;
+// var green = prompt("Please give me value for green parameter") || 0;
+// var blue = prompt("Please give me value for blue parameter") || 0;
 
-function rgb(red, green, blue) {
-  return console.log("rgb(" + red + "," + green + "," + blue + ")");
-}
+// function rgb(red, green, blue) {
+//   return console.log("rgb(" + red + "," + green + "," + blue + ")");
+// }
 
-rgb(red, green, blue);
-
-// 0,5,6,7,8,9,10,11,12,15,20,25,26,27,30,35,40, - товаров
-  // 1,21,31,41 - товар
-  // 2,3,4,22,23,24,32,33,34 - товара
-  //до 50
-  
-var numberOfItems = prompt("How many items?");
+// rgb(red, green, blue);
 
 function declension(numberOfItems) {
-  var word = "товар";
-  
-    if (numberOfItems === 1 || 21 || 31 || 41) {
-    return console.log(numberOfItems + word);
+  var word = " товар";
+
+  if (
+    numberOfItems === 1 ||
+    numberOfItems === 21 ||
+    numberOfItems === 31 ||
+    numberOfItems === 41
+  ) {
+    console.log(numberOfItems + word);
+    return numberOfItems + word;
+  } else if (
+    numberOfItems === 0 ||
+    (numberOfItems >= 5 && numberOfItems <= 20) ||
+    (numberOfItems >= 25 && numberOfItems <= 30) ||
+    (numberOfItems >= 35 && numberOfItems <= 40) ||
+    (numberOfItems >= 45 && numberOfItems <= 50)
+  ) {
+    console.log(numberOfItems + word + "ов");
+    return numberOfItems + word + "ов";
+  } else {
+    console.log(numberOfItems + word + "а");
+    return numberOfItems + word + "а";
+  }
 }
-    else if (numberOfItems === 0 || (numberOfItems >= 5 && numberOfItems <= 20) || (numberOfItems >= 25 && numberOfItems <= 30) || (numberOfItems >=35 && numberOfItems <= 40) || (numberOfItems >= 45 && numberOfItems <= 50)) {
-        return console.log(numberOfItems + word + "ов");
-    } else  
-    return console.log(numberOfItems + word + "а");
-}
-declension(numberOfItems);
-// } else  
-// return console.log(numberOfItems + word + "а"); - не реагирует и при проверке выдает 'товар'
+
+declension(0);
